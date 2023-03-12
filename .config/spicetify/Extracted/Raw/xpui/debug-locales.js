3eccbe700ef7bb2c2690ae8bf64290b7a36c5bf3
Princypal1 <98353149+Princypal1@users.noreply.github.com>
2023-03-12 16:06:45 +0530

    Delete .config/spicetify directory
    Did not need it in the repo.

--- .config/spicetify/Extracted/Raw/xpui/debug-locales.js
+++
@@ -1,2 +0,0 @@
-"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[6658],{87691:(e,l,f)=>{f.r(l),f.d(l,{default:()=>n});const n=f(11108).R}}]);
-//# sourceMappingURL=debug-locales.js.map
